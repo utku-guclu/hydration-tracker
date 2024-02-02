77960fdaa5cd991659df593dda39b67cc67cbd9f
utku-guclu <utku_guclu@hotmail.com>
2024-02-02 23:13:50 +0300

    user authentication

--- server/app.js
+++ server/app.js
@@ -1,127 +1,89 @@
-// server.js or index.js
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const { PrismaClient } = require("@prisma/client");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+/* controllers */
+const userController = require("./controllers/userController");
+const authController = require("./controllers/authController");
+const hydrationLogController = require("./controllers/hydrationLogController");
 
+/* database */
 const prisma = new PrismaClient();
 
+/* port */
+const PORT = process.env.PORT || 3000;
+
+/* app */
+const app = express();
+
+/* secret-key */
+const secretKey = process.env.JWT_SECRET_KEY;
+
+/* helmet configuration with various security options */
+app.use(
+  helmet({
+    frameguard: {
+      // Set frameguard to deny to prevent clickjacking attacks
+      action: "deny",
+    },
+    contentSecurityPolicy: {
+      // Enable and configure Content Security Policy (CSP) to restrict sources for loading content
+      directives: {
+        defaultSrc: ["'self'"], // Allow content from the same origin
+        styleSrc: ["style.com"], // Allow styles from 'style.com'
+      },
+    },
+    dnsPrefetchControl: false, // Disable DNS prefetching
+  })
+);
+
+/* Hide the 'X-Powered-By' header to reduce information disclosure */
+app.use(helmet.hidePoweredBy());
+
+/* Set frameguard to deny to prevent clickjacking attacks */
+app.use(helmet.frameguard({ action: "deny" }));
+
+/* Enable XSS (Cross-Site Scripting) filter */
+app.use(helmet.xssFilter());
+
+/* Set 'X-Content-Type-Options' header to 'nosniff' to prevent MIME sniffing */
+app.use(helmet.noSniff());
+
+/* Set 'X-Download-Options' header to 'noopen' to prevent Internet Explorer from executing downloads in the context of the web page */
+app.use(helmet.ieNoOpen());
+
+/* Enable HSTS (HTTP Strict Transport Security) with a max-age of 90 days and force HTTPS */
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+const timeInSeconds = ninetyDaysInSeconds;
+
+app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
+
+/* Enable DNS prefetch control to control browser DNS prefetching behavior */
+app.use(helmet.dnsPrefetchControl());
+
+/* Content Security Policy (CSP) to restrict sources for loading scripts */
+app.use(
+  helmet.contentSecurityPolicy({
+    defaultSrc: ["'self'"], // Allow scripts from the same origin
+    scriptSrc: ["'self'", "'cdnjs.cloudflare.com'"], // Allow scripts from the same origin and 'trusted-cdn'
+  })
+);
+
+/* cors */
 app.use(cors());
+
+/* json */
 app.use(express.json());
 
-app.post("/api/hydration-logs", async (req, res) => {
-    const { intake } = req.body;
-
-    try {
-        const createdLog = await prisma.hydrationLog.create({
-            data: {
-                intake,
-            },
-        });
-
-        console.log("Hydration log added successfully");
-        res.status(201).json({
-            message: "Hydration log added successfully",
-        });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-app.get("/api/hydration-logs", async (req, res) => {
-    try {
-        const logs = await prisma.hydrationLog.findMany({
-            select: {
-                intake: true,
-                timestamp: true,
-            },
-            orderBy: {
-                timestamp: "desc",
-            },
-        });
-
-        res.status(200).json(logs);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// DELETE endpoint to remove a hydration log by timestamp
-// DELETE endpoint to remove a hydration log by timestamp
-app.delete("/api/hydration-logs/:timestamp", async (req, res) => {
-    const { timestamp } = req.params;
-
-    try {
-        // Find the hydration log with the given timestamp
-        const existingLog = await prisma.hydrationLog.findFirst({
-            where: {
-                timestamp: new Date(timestamp),
-            },
-        });
-
-        // If the log exists, delete it using the timestamp
-        if (existingLog) {
-            await prisma.hydrationLog.delete({
-                where: {
-                    id: existingLog.id,
-                },
-            });
-
-            console.log("Hydration log deleted successfully");
-            res.status(204).end();
-        } else {
-            // If the log does not exist, return a 404 response
-            console.error("Hydration log not found");
-            res.status(404).json({ error: "Hydration log not found" });
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// PUT endpoint to update a hydration log by timestamp
-app.put("/api/hydration-logs/:timestamp", async (req, res) => {
-    const { timestamp } = req.params;
-    const { intake } = req.body;
-
-    try {
-        // Find the hydration log with the given timestamp
-        const existingLog = await prisma.hydrationLog.findFirst({
-            where: {
-                timestamp: new Date(timestamp),
-            },
-        });
-
-        // If the log exists, update it using the timestamp
-        if (existingLog) {
-            const updatedLog = await prisma.hydrationLog.update({
-                where: {
-                    id: existingLog.id,
-                },
-                data: {
-                    intake,
-                },
-            });
-
-            console.log("Hydration log updated successfully");
-            res.status(200).json(updatedLog);
-        } else {
-            // If the log does not exist, return a 404 response
-            console.error("Hydration log not found");
-            res.status(404).json({ error: "Hydration log not found" });
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
+/* ENDPOINTS */
+app.use("/api/users", userController);
+app.use("/api", authController); // For user login and registration
+app.use("/api/hydration-logs", hydrationLogController);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+  
