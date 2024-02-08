290fd593af2a33edd73ffde99101c15e3130bdf3
utku-guclu <utku_guclu@hotmail.com>
2024-02-08 23:11:27 +0300

    vite:conf-github:workflow

--- server/prisma/migrations/20240124223932_hydration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "HydrationLog" (
-    "id" SERIAL NOT NULL,
-    "intake" INTEGER NOT NULL,
-    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "HydrationLog_pkey" PRIMARY KEY ("id")
-);
--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/authController");
-
-const authRoutes = express.Router();
-
-authRoutes.use("/login", authController);
-
-module.exports = authRoutes;
