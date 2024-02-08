7d13f8622ad966ead8fa3add9a60039adb71d7c0
Utku <34550887+utku-guclu@users.noreply.github.com>
2024-02-08 23:53:34 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import styled from "styled-components";
+import { styled } from "@mui/system";
 
 /* Semantic Header */
 const StyledHeader = styled.header`
