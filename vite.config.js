4e9980d327ee32eba26af5eb7f23613418a7e19c
utku-guclu <utku_guclu@hotmail.com>
2024-02-12 22:58:08 +0300

    accept development changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-<<<<<<< HEAD
 import { qrcode } from "vite-plugin-qrcode";
-=======
->>>>>>> ceab142 (fixed conflicts and merged development)
 
 // https://vitejs.dev/config/
 export default defineConfig({
