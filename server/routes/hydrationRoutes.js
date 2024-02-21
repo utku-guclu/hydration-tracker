b7c25be2ab21aaf917d9ac34cb047350eb09eea1
utku-guclu <utku_guclu@hotmail.com>
2024-02-21 20:56:52 +0300

    package update

--- server/routes/hydrationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const hydrationLogController = require("../controllers/hydrationLogController");
-
-const hydrationLogRoutes = express.Router();
-
-hydrationLogRoutes.use("/logs", hydrationLogController);
-
-module.exports = hydrationLogRoutes;
