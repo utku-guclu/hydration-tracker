3d81e02cabcfb78b36bb454afab0c6760b623b24
utku-guclu <utku_guclu@hotmail.com>
2024-02-28 01:58:18 +0300

    client-id expose

--- src/components/User/Google/SignIn.jsx
+++ src/components/User/Google/SignIn.jsx
@@ -5,6 +5,8 @@ import useFetch from "../../../hooks/useFetch";
 import server from "../../../config/baseURL";
 import { CircularProgress } from "@mui/material";
 
+import googleClient from "../../../config/googleClient";
+
 // https://developers.google.com/identity/gsi/web/reference/js-reference
 
 import GoogleButton from "react-google-button";
--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -5,3 +5,4 @@ const server =
     ? "https://hydration-5xayjtz4za-ew.a.run.app"
     : "http://localhost:3000";
 export default server;
+
